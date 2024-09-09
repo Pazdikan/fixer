b516f95a04ccf7ae2c6407e16f73fab29f24b017
Pazdikan <pazdikan@gmail.com>
2024-09-09 09:08:10 +0200

    refactor: Remove unused variables

--- src/app.tsx
+++ src/app.tsx
@@ -1,11 +1,6 @@
-import { useState } from 'preact/hooks'
-import preactLogo from './assets/preact.svg'
-import viteLogo from '/vite.svg'
 import './app.css'
 
 export function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
       <div className="m-24">
