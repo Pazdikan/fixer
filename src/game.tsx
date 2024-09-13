6575af85c79a5ba40e898ddd3da3f7507ad5df1d
Pazdikan <pazdikan@gmail.com>
2024-09-13 12:23:45 +0200

    feat: Install shadcn and lucide

--- src/game.tsx
+++ src/game.tsx
@@ -1,6 +1,7 @@
 import InfoBanner from './components/InfoBanner';
 import GameHomePage from './pages/GameHomePage';
 import WelcomeScreen from './pages/WelcomePage'
+import { Button } from "@/components/ui/button"
 
 export function App() {
   const isPlayerNew: boolean = JSON.parse(window.localStorage.getItem("isPlayerNew") || "true");
--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,14 @@
-import { defineConfig } from 'vite'
+import path from "path"
+import { defineConfig } from "vite"
 import preact from '@preact/preset-vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [preact()],
-  base: '/fixer/'
+  base: '/fixer/',
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
 })
