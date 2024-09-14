b96f8061b7a9d307ac429786f1307e47aab65800
Pazdikan <pazdikan@gmail.com>
2024-09-14 20:13:11 +0200

    feat(lang): Add translation-check for a quick-peek at translations

--- src/lib/i18n.ts
+++ src/lib/i18n.ts
@@ -2,13 +2,15 @@ import i18n from 'i18next';
 import ICU from 'i18next-icu';
 import LanguageDetector from 'i18next-browser-languagedetector';
 import { initReactI18next  } from 'react-i18next';
+import { i18nextPlugin } from 'translation-check'
 
 import translation_en from "../../locales/en/translation.json";
 
 i18n
   .use(ICU)
   .use(LanguageDetector)
-  .use(initReactI18next )
+  .use(initReactI18next)
+  .use(i18nextPlugin)
   .init({
     fallbackLng: 'en',
     lng: "en",
