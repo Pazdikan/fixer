5c7e35969e9bbad909ef48a4f343a48e2b22ff63
Pazdikan <pazdikan@gmail.com>
2024-09-18 11:09:20 +0200

    feat: Ticks

---
+++ src/core/brain.tsx
@@ -0,0 +1,21 @@
+export class GameBrain {
+  private static tickInterval: number = 1000 / 10; // 10 ticks per second
+  private static intervalId: number | null = null;
+
+  public static start() {
+    if (this.intervalId === null) {
+      this.intervalId = window.setInterval(this.tick, this.tickInterval);
+    }
+  }
+
+  public static stop() {
+    if (this.intervalId !== null) {
+      window.clearInterval(this.intervalId);
+      this.intervalId = null;
+    }
+  }
+
+  private static tick() {
+    console.log('Tick');
+  }
+}
--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,10 @@ import './lib/i18n.ts'
 import { GameProvider } from './components/game/game-context.tsx'
 import GameHomePage from '@/components/pages/GameHomePage.tsx'
 
+import { GameBrain } from '@/core/brain';
+
 render(<GameProvider>
     <GameHomePage />
 </GameProvider>, document.getElementById('app')!)
+
+GameBrain.start();
