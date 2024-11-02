ce90230639e9da0152eefef237c18329414b1e39
Pazdikan <pazdikan@gmail.com>
2024-11-02 15:22:49 +0100

    build: Add no-default-export eslint rule

--- eslint.config.js
+++ eslint.config.js
@@ -1,10 +1,14 @@
 import js from "@eslint/js";
+import importPlugin from "eslint-plugin-import";
 
 export default [
   js.configs.recommended,
+  importPlugin.flatConfigs.recommended,
   {
     rules: {
       "no-unused-vars": "off",
+      "no-undef": "off",
+      "import/no-default-export": "error",
     },
   },
 ];
