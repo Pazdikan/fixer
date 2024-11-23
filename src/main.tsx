a4a9c606e57305bcd0342bd2ec15e4dbc1a8a4f8
Pazdikan <pazdikan@gmail.com>
2024-11-23 12:11:54 +0100

    feat: Reimplement i18n

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,7 @@
 import { createRoot, Root } from "react-dom/client";
 
 import "./index.css";
+import "@/common/lib/i18n";
 import { useGame } from "./core/store/game-store";
 import { GameRoot } from "./common/components/root/root";
 import { addonManager } from "./addon/addon";
