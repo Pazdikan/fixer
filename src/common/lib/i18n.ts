a9a1bbaba3d990421ad7e9ee01f64365b7a9746a
Pazdikan <pazdikan@gmail.com>
2025-02-04 21:34:11 +0100

    refactor: restructure localization files, update i18n imports
    - Remove old localization files for English and Polish.
    - Add new structured localization files for messages in English and Polish.
    - Update i18n configuration to use new message files.

--- src/common/lib/i18n.ts
+++ src/common/lib/i18n.ts
@@ -4,8 +4,8 @@ import LanguageDetector from "i18next-browser-languagedetector";
 import { initReactI18next } from "react-i18next";
 import { i18nextPlugin } from "translation-check";
 
-import translation_en from "../../../locales/en.json";
-import translation_pl from "../../../locales/pl.json";
+import translation_en_messages from "../../../locales/en/messages.json";
+import translation_pl_messages from "../../../locales/pl/messages.json";
 
 i18n
   .use(ICU)
@@ -18,10 +18,10 @@ i18n
     debug: true,
     resources: {
       en: {
-        translation: translation_en,
+        translation: translation_en_messages,
       },
       pl: {
-        translation: translation_pl,
+        translation: translation_pl_messages,
       },
     },
   });
